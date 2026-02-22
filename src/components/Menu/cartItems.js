6faeb4fc129da4fe178a9ba6331b303458643466
Rajeev Negi <rajeevnegi005@gmail.com>
2026-02-22 14:48:33 +0530

    add to cart feature is under development

--- src/components/Menu/cartItems.js
+++ src/components/Menu/cartItems.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 export default function CartItems({ item }) {
-  const [cartItems, setCartItems] = useState([]);
+  // const [cartItems, setCartItems] = useState([]);
 
   // console.log(item);
 
   // Use an effect to add the 'item' prop to 'cartItems' when the component mounts
   // or when the 'item' prop itself changes.
-  useEffect(() => {
-    if (item) {
-      setCartItems((prevItems) => [...prevItems, item]);
-      // console.log("item added!");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (item) {
+  //     setCartItems((prevItems) => [...prevItems, item]);
+  //     // console.log("item added!");
+  //   }
+  // }, []);
   // console.log(cartItems);
 
   return (
     <div>
-      {cartItems.map((item) => {
+      {/* {cartItems.map((item) => {
         return <p>{item.name}</p>;
-      })}
+      })} */}
     </div>
   );
 }
